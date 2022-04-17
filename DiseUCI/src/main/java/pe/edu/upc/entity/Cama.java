38469d2d1108d01bed72bc7635d6e75357086df3
FlaviaPardo <flavia.pardval@gmail.com>
2022-04-17 11:03:47 -0500

    cambio2

--- DiseUCI/src/main/java/pe/edu/upc/entity/Cama.java
+++ DiseUCI/src/main/java/pe/edu/upc/entity/Cama.java
@@ -9,7 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
-
 @Entity
 @Table(name="cama")
 public class Cama implements Serializable {
