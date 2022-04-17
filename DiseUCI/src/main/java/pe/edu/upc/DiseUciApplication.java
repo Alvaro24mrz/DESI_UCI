79dd71b3da9fd6920e12949969b284c873a03f02
Natalia Villanueva <villanuevaznatalia@gmail.com>
2022-04-16 19:23:01 -0500

    Archivo

---
+++ DiseUCI/src/main/java/pe/edu/upc/DiseUciApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiseUciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiseUciApplication.class, args);
+	}
+
+}
